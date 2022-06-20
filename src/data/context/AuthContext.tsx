96bf000ce958e02bd85649b19c85ab865b96ec13
augusto <joaoaugustojob99@gmail.com>
2022-06-20 16:39:24 -0300

    start authentication

--- src/data/context/AuthContext.tsx
+++ src/data/context/AuthContext.tsx
@@ -1,1 +1,29 @@
 import firebase from '../../firebase/config';
+
+import { createContext, useState } from 'react';
+import { User } from '../../model/User';
+
+interface AuthContextProps {
+  user?: User;
+  loginGoogle?: () => Promise<void>;
+}
+
+interface AuthProvider {
+  children: React.ReactNode;
+}
+
+export const AuthContext = createContext<AuthContextProps>({} as AuthContextProps);
+
+export function AuthProvider({ children }: AuthProvider) {
+  const [user, setUser] = useState<User>(null);
+
+  async function loginGoogle() {
+    setUser('')
+  }
+
+  return (
+    <AuthContext.Provider value={{ user, loginGoogle }}>
+      {children}
+    </AuthContext.Provider>
+  )
+}
---
+++ src/model/User.ts
@@ -0,0 +1,8 @@
+export interface User {
+  uid: string;
+  email: string;
+  name: string;
+  token: string;
+  provider: string;
+  imageUrl: string;
+} 
